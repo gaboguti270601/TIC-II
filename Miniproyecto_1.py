950f5b66688261c3c9127e2c487ae7312aa6c6b3
gaboguti270601 <81125775+gaboguti270601@users.noreply.github.com>
2023-06-05 16:32:46 -0400

    Update Miniproyecto_1.py

--- Miniproyecto_1.py
+++ Miniproyecto_1.py
@@ -1,10 +1,17 @@
 import time
 import board
 import adafruit_dht
+import logging
 
 # Initialize the dht device with the data pin connected to pin 16 (GPIO 23) of the Raspberry Pi:
 dhtDevice = adafruit_dht.DHT11(board.D18)
 
+# Configurar la configuración del logging
+logging.basicConfig(
+    filename='log.txt', # Nombre y ubicación del archivo de logging
+    level=logging.INFO, # Nivel de registro mínimo
+    format='%(asctime)s - %(levelname)s - %(message)s' # Formato de registro
+)
 while True:
     try:
         # Print the values via the serial interface
@@ -20,3 +27,9 @@ while True:
         dhtDevice.exit()
         raise error
     time.sleep(2.0)
+# Registrar eventos de ejemplo
+logging.debug('Este es un mensaje de depuración')
+logging.info('Este es un mensaje informativo')
+logging.warning('Este es un mensaje de advertencia')
+logging.error('Este es un mensaje de error')
+logging.critical('Este es un mensaje crítico')
