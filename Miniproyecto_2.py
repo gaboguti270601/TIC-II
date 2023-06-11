1360c0eaad688633deca256298ee4c632bd1fc1e
gaboguti270601 <81125775+gaboguti270601@users.noreply.github.com>
2023-06-11 01:50:57 -0400

    Update Miniproyecto_2.py

--- Miniproyecto_2.py
+++ Miniproyecto_2.py
@@ -3,33 +3,33 @@ import board
 import adafruit_dht
 import logging
 
-# Configuración del logging.
+# Configuramos el logging.
 logging.basicConfig(
     filename='log.txt',  # Nombre y ubicación del archivo de logging.
     level=logging.INFO,  # Nivel de registro mínimo.
     format='%(asctime)s - %(levelname)s - %(message)s'  # Formato de registro.
 )
 
-# Inicializar el dispositivo dht con el pin de datos conectado al pin GPIO 23 de la Raspberry Pi:
+# Inicializamos el dispositivo dht con el pin de datos conectado al pin GPIO 23 de la Raspberry Pi:
 dhtDevice = adafruit_dht.DHT11(board.D18)
 
 while True:
     try:
-        # Obtener la marca de tiempo actual.
+        # Obtenemos la marca de tiempo actual.
         Tiempo = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-        # Leer los valores del sensor.
+        # Leemos los valores del sensor.
         Temperatura_c = dhtDevice.temperature
         Humedad = dhtDevice.humidity
 
-        # Crear descripción del texto.
+        # Creamos descripción del texto.
         texto = "Temperatura: {:.1f} C    Humedad: {}%".format(Temperatura_c, Humedad)
 
-        # Registrar el texto en el archivo de log
+        # Registramos el texto en el archivo de log
         logging.info("{} - {}".format(Tiempo, texto))
 
     except RuntimeError as error:
-        # Registrar el mensaje de error en el archivo de log
+        # Registramos el mensaje de error en el archivo de log
         logging.error("{} - {}".format(Tiempo, error.args[0]))
 
     except Exception as error:
