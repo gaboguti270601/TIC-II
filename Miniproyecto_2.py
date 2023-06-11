8ba26da4484feab3ab1f430a1e46644b286ff434
gaboguti270601 <81125775+gaboguti270601@users.noreply.github.com>
2023-06-11 00:43:21 -0400

    Update Miniproyecto_2.py

--- Miniproyecto_2.py
+++ Miniproyecto_2.py
@@ -3,34 +3,34 @@ import board
 import adafruit_dht
 import logging
 
-# Configurar la configuración del logging
+# Configuración del logging.
 logging.basicConfig(
-    filename='log.txt',  # Nombre y ubicación del archivo de logging
-    level=logging.INFO,  # Nivel de registro mínimo
-    format='%(asctime)s - %(levelname)s - %(message)s'  # Formato de registro
+    filename='log.txt',  # Nombre y ubicación del archivo de logging.
+    level=logging.INFO,  # Nivel de registro mínimo.
+    format='%(asctime)s - %(levelname)s - %(message)s'  # Formato de registro.
 )
 
-# Initialize the dht device with the data pin connected to pin 16 (GPIO 23) of the Raspberry Pi:
+# Inicializar el dispositivo dht con el pin de datos conectado al pin GPIO 23 de la Raspberry Pi:
 dhtDevice = adafruit_dht.DHT11(board.D18)
 
 while True:
     try:
-        # Obtener la marca de tiempo actual
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        # Obtener la marca de tiempo actual.
+        Tiempo = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-        # Leer los valores del sensor
-        temperature_c = dhtDevice.temperature
-        humidity = dhtDevice.humidity
+        # Leer los valores del sensor.
+        Temperatura_c = dhtDevice.temperature
+        Humedad = dhtDevice.humidity
 
-        # Crear la descripción del evento con los valores leídos
-        event_description = "Temp: {:.1f} C    Humidity: {}%".format(temperature_c, humidity)
+        # Crear descripción del texto.
+        texto = "Temperatura: {:.1f} C    Humedad: {}%".format(Temperatura_c, Humedad)
 
-        # Registrar el evento en el archivo de log
-        logging.info("{} - {}".format(timestamp, event_description))
+        # Registrar el texto en el archivo de log
+        logging.info("{} - {}".format(Tiempo, texto))
 
     except RuntimeError as error:
         # Registrar el mensaje de error en el archivo de log
-        logging.error("{} - {}".format(timestamp, error.args[0]))
+        logging.error("{} - {}".format(Tiempo, error.args[0]))
 
     except Exception as error:
         dhtDevice.exit()
