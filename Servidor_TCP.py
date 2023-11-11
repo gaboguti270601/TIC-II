ac41e51ccd25131dd815793a9d667550382b6130
gaboguti270601 <81125775+gaboguti270601@users.noreply.github.com>
2023-11-11 12:40:05 -0300

    Add files via upload

---
+++ Servidor_TCP.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 1234
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    s.listen()
+    
+    print("El servidor está esperando conexiones en el puerto" , PORT)
+    
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print('Conectdo por', addr[0])
+            data = conn.recv(1024)
+            if data:
+                print("Recibido: ", data.decode('utf-8'))
+                respuesta = "Tu mensaje es " +data.decode('utf-8')
+                conn.sendall(respuesta.encode('utf-8'))
