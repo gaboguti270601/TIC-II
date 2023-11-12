6d554325921662d1a10ab46e13e322f00ad39827
gaboguti270601 <81125775+gaboguti270601@users.noreply.github.com>
2023-11-12 13:07:22 -0300

    Update Cliente_TCP.c

--- Cliente_TCP.c
+++ Cliente_TCP.c
@@ -14,18 +14,16 @@ void socket_tcp() {
     uint8_t mac[6];
     esp_wifi_get_mac(WIFI_IF_STA, mac);
 
-    // Crear una variable para la dirección MAC
-    char mac_str[18];  // 6 bytes en formato hexadecimal y 5 guiones
-    snprintf(mac_str, sizeof(mac_str), "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    // Crear una variable para el ID_Device utilizando los primeros 2 bytes de la dirección MAC
+    char id_device[5];
+    snprintf(id_device, sizeof(id_device), "%02x%02x", mac[0], mac[1]);
 
-    // Crear una variable para el dato
-    const char *dato = "Hola Mundo";
 
-    // Calcular el largo del dato
-    int dato_length = strlen(dato);
+    // Crear una variable para el mensaje
+    const char *mensaje = "Hola Mundo";
 
-    // Crear una variable para el ID_Device
-    const char *ID_Device = "03";
+    // Calcular el largo del mensaje
+    int mensaje_length = strlen(mensaje);
 
     // Crear una variable para el Transport_layer
     const char *Transport_layer = "0";
@@ -33,9 +31,13 @@ void socket_tcp() {
     // Crear una variable para el ID_Protocol
     const char *ID_Protocol = "7";
 
-    // Crear un formato de mensaje que incluya el ID_Device, la dirección MAC, el Transport_layer, el ID_Protocol, el largo del dato y el dato
-    char message[80];  // Aumentar el tamaño del buffer si es necesario
-    snprintf(message, sizeof(message), "%s - %s - %s - %s - %d - %s", ID_Device, mac_str, Transport_layer, ID_Protocol, dato_length, dato);
+    // Crear una variable para la dirección MAC
+    char mac_str[18];  // 6 bytes en formato hexadecimal y 5 guiones
+    snprintf(mac_str, sizeof(mac_str), "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+    // Crear un formato de mensaje que incluya el ID_Device, la dirección MAC, el Transport_layer, el ID_Protocol, el largo del mensaje y el mensaje
+    char message[128];
+    snprintf(message, sizeof(message), "%s - %s - %s - %s - %d - %s", id_device, mac_str, Transport_layer, ID_Protocol, mensaje_length, mensaje);
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT_TCP);
@@ -55,7 +57,7 @@ void socket_tcp() {
         return;
     }
 
-    // Enviar el mensaje que incluye el ID_Device, la dirección MAC, el Transport_layer, el ID_Protocol, el largo del dato y el dato al servidor
+    // Enviar el mensaje que incluye el ID_Device, la dirección MAC, el Transport_layer, el ID_Protocol, el largo del mensaje y el mensaje al servidor
     send(sock, message, strlen(message), 0);
 
     // Recibir respuesta
