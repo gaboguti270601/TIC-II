43387baad1975ca07ccaa984c1ddad7e2e9059c8
gaboguti270601 <81125775+gaboguti270601@users.noreply.github.com>
2023-11-12 17:44:44 -0300

    Update Leer_json.py

--- Leer_json.py
+++ Leer_json.py
@@ -5,7 +5,8 @@ def leer_mensajes():
         with open('mensajes.json', 'r') as file:
             mensajes = json.load(file)
             return mensajes
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
+    except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+        print(f"Error al leer el archivo JSON: {e}")
         return []
 
 def main():
@@ -14,7 +15,13 @@ def main():
     if mensajes:
         print("Mensajes almacenados:")
         for mensaje in mensajes:
-            print(f"{mensaje['tipo']} de {mensaje['direccion']}: {mensaje['mensaje']}")
+            tipo = mensaje.get('tipo', 'Tipo no especificado')
+            direccion = mensaje.get('direccion', 'Dirección no especificada')
+            mensaje_data = mensaje.get('mensaje', {})
+            header = mensaje_data.get('Header', 'Header no especificado')
+            datos = mensaje_data.get('Datos', 'Datos no especificados')
+            
+            print(f"{tipo} de {direccion}: Header: {header}, Datos: {datos}")
     else:
         print("No hay mensajes almacenados.")
 
